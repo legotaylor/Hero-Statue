b3e94bedc57616f4ed236051f937664cb43d9f1a
dannytaylor <daniel@mclegoman.com>
2025-08-08 01:47:38 +1200

    we haven't used a single mixin

--- src/main/java/dev/dannytaylor/hero_statue/client/render/pipeline/RenderPipelineRegistry.java
+++ src/main/java/dev/dannytaylor/hero_statue/client/render/pipeline/RenderPipelineRegistry.java
@@ -9,6 +9,8 @@ package dev.dannytaylor.hero_statue.client.render.pipeline;
 
 import com.mojang.blaze3d.pipeline.BlendFunction;
 import com.mojang.blaze3d.pipeline.RenderPipeline;
+import com.mojang.blaze3d.platform.DestFactor;
+import com.mojang.blaze3d.platform.SourceFactor;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import dev.dannytaylor.hero_statue.client.compatibility.HeroStatueIris;
 import dev.dannytaylor.hero_statue.common.data.CommonData;
@@ -22,6 +24,6 @@ public class RenderPipelineRegistry {
 		HeroStatueIris.registerPipelines();
 	}
 	static {
-		statueEyes = RenderPipelines.register(RenderPipeline.builder(RenderPipelines.TRANSFORMS_PROJECTION_FOG_SNIPPET).withLocation(CommonData.idOf("pipeline/statue_eyes")).withVertexShader(CommonData.idOf("core/statue_eyes")).withFragmentShader(CommonData.idOf("core/statue_eyes")).withSampler("Sampler0").withBlend(BlendFunction.ADDITIVE).withDepthWrite(false).withVertexFormat(VertexFormats.POSITION_COLOR_TEXTURE_LIGHT_NORMAL, VertexFormat.DrawMode.QUADS).withDepthBias(-2.0F, -20.0F).build());
+		statueEyes = RenderPipelines.register(RenderPipeline.builder(RenderPipelines.TRANSFORMS_PROJECTION_FOG_SNIPPET).withLocation(CommonData.idOf("pipeline/statue_eyes")).withVertexShader(CommonData.idOf("core/statue_eyes")).withFragmentShader(CommonData.idOf("core/statue_eyes")).withSampler("Sampler0").withBlend(new BlendFunction(SourceFactor.ONE, DestFactor.ONE_MINUS_SRC_COLOR)).withDepthWrite(false).withVertexFormat(VertexFormats.POSITION_COLOR_TEXTURE_LIGHT_NORMAL, VertexFormat.DrawMode.QUADS).withDepthBias(-1.0F, -5.0F).build());
 	}
 }
